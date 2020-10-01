1b5fdf485366b26193b5a779032235e6536949b2
Retrokiller543 <emil.schutt@gmail.com>
2020-10-01 13:27:41 +0200

    done with the prototype

--- FaceRecognition/script.js
+++ FaceRecognition/script.js
@@ -5,59 +5,77 @@ Promise.all([
   faceapi.nets.faceLandmark68Net.loadFromUri("/models"),
   faceapi.nets.faceRecognitionNet.loadFromUri("/models"),
   faceapi.nets.faceExpressionNet.loadFromUri("/models"),
-    faceapi.nets.ssdMobilenetv1.loadFromUri('/models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri("/models"),
 ]).then(startVideo);
 
 function startVideo() {
   navigator.getUserMedia(
     { video: {} },
-    (stream) => (video.srcObject = stream), 
+    (stream) => (video.srcObject = stream),
     (err) => console.error(err)
   );
 }
 
-video.addEventListener("play", () => {
+video.addEventListener("play", async () => {
   const canvas = faceapi.createCanvasFromMedia(video);
   document.body.append(canvas);
-      const labeledFaceDescriptors = await loadLabeledImages()
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-
   const displaySize = { width: video.width, height: video.height };
   faceapi.matchDimensions(canvas, displaySize);
+  const labeledFaceDescriptors = await loadLabeledImages();
+  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6);
+    console.log('loaded');
   setInterval(async () => {
+    // const detections = await faceapi
+    //   .detectAllFaces(video)
+    //   .withFaceLandmarks()
+    //   .withFaceExpressions()
+    //   .withFaceDescriptor();
     const detections = await faceapi
-      .detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+      .detectAllFaces(video)
       .withFaceLandmarks()
-      .withFaceExpressions().withFaceDescriptor();
+      .withFaceDescriptors();
     const resizedDetections = faceapi.resizeResults(detections, displaySize);
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-            results.forEach((result, i) => {
-            const box = resizedDetections[i].detection.box
-            const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-            drawBox.draw(canvas)
-            canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
-    faceapi.draw.drawDetections(canvas, resizedDetections);
-    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-        })
-  }, 100);
+    const results = resizedDetections.map((d) =>
+      faceMatcher.findBestMatch(d.descriptor)
+    );
+    results.forEach((result, i) => {
+      const box = resizedDetections[i].detection.box;
+      const drawBox = new faceapi.draw.DrawBox(box, {
+        label: result.toString(),
+      });
+      canvas.getContext("2d").clearRect(0, 0, canvas.width, canvas.height);
+      drawBox.draw(canvas);
+      faceapi.draw.drawDetections(canvas, resizedDetections);
+    //   faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    //   faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+    });
+  }, 50);
 });
 
 function loadLabeledImages() {
-    const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark', 'Emil Schütt']
-    return Promise.all(
-        labels.map(async label => {
-            const descriptions = []
-            for (let i = 1; i <= 2; i++) {
-                const img = await faceapi.fetchImage(
-                  `https://raw.githubusercontent.com/Retrokiller543/Face-Recognition/master/Face%20rec%202/labeled_images/${label}/${i}.jpg`,
-                  { mode: "no-cors" }
-                );
-                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
-                descriptions.push(detections.descriptor)
-            }
+  const labels = [
+    "Emil Schütt",
+    'Marcus Nykvist',
+    'Alexander Franke',
+    'Sofia',
+    'Stella'
+  ];
+  return Promise.all(
+    labels.map(async (label) => {
+      const descriptions = [];
+      for (let i = 1; i <= 2; i++) {
+        const img = await faceapi.fetchImage(
+          `https://raw.githubusercontent.com/Retrokiller543/Face-Recognition/master/Face%20rec%202/labeled_images/${label}/${i}.jpg`,
+          { mode: "no-cors" }
+        );
+        const detections = await faceapi
+          .detectSingleFace(img)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        descriptions.push(detections.descriptor);
+      }
 
-            return new faceapi.LabeledFaceDescriptors(label, descriptions)
-        })
-    )
+      return new faceapi.LabeledFaceDescriptors(label, descriptions);
+    })
+  );
 }
